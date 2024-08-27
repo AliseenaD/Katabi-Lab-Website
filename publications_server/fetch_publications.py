a1f840589c710dac33a1a564e73350ac04de0f22
alimirz <alimirz@emeraldinno.com>
2024-08-27 09:09:19 -0400

    changed api key

--- publications_server/fetch_publications.py
+++ publications_server/fetch_publications.py
@@ -1,7 +1,7 @@
 import requests
 import json
 # Define the URL
-url = "https://serpapi.com/search.json?engine=google_scholar_author&author_id=nst5fHgAAAAJ&sort=pubdate&api_key=d86b2a87580e82eacab17b965bd7fdd083d6f7b3e42799c66550bcef4c5d5b44"
+url = "https://serpapi.com/search.json?engine=google_scholar_author&author_id=nst5fHgAAAAJ&sort=pubdate&api_key=4c1902d6e9cf12346fcf8515fd331280c1f63ab7bb1a40cd39b9bd075af93023"
 
 # Send the request and fetch the data
 try:
--- publications_server/publications_server.mjs
+++ publications_server/publications_server.mjs
@@ -7,7 +7,7 @@ app.use(cors());
 
 
 app.get('/publications', async (req, res) => {
-  const url = `https://serpapi.com/search.json?engine=google_scholar_author&author_id=nst5fHgAAAAJ&sort=pubdate&api_key=d86b2a87580e82eacab17b965bd7fdd083d6f7b3e42799c66550bcef4c5d5b44`;
+  const url = `https://serpapi.com/search.json?engine=google_scholar_author&author_id=nst5fHgAAAAJ&sort=pubdate&api_key=4c1902d6e9cf12346fcf8515fd331280c1f63ab7bb1a40cd39b9bd075af93023`;
 
   try {
     const response = await fetch(url);
